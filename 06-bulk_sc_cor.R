09a60109e75889b3025b966fc3354e776790a553
Bhavya Singh <singhb39@mcmaster.ca>
2022-09-22 09:53:14 -0400

    tissue specific colors

--- 06-bulk_sc_cor.R
+++ 06-bulk_sc_cor.R
@@ -26,7 +26,24 @@ load("r_outputs/03-mean_raw_scTE_cpm_by_tissue_type.RData")
 row.names(samples) <- samples$sn_RNAseq
 samples$sn_RNAseq_names <- gsub("-", "_", samples$sn_RNAseq)
 
+################################# COLOR SETUP ##################################
+
+mypal = pal_npg("nrc", alpha = 0.7)(8)
+mycols <- c("#E64B35B2","#4DBBD5B2","#00A087B2","#3C5488B2",
+            "#F39B7FB2","#8491B4B2","#91D1C2B2","#DC0000B2")
+
+tissues <- c("Prostate", "Heart", "Lung", "E_Muscularis",
+             "E_Mucosa" , "Sk_muscle", "Skin", "Breast")
+
+tissue_color<- data.frame(tissues, mycols)
+
+samples$color <- 
+  tissue_color$mycols[match(samples$tissue, tissue_color$tissues)]
+
 ############################# SC / BULK CORRELATION ############################
+################################# SAMPLE LEVEL #################################
+
+all_bulk_sc <- data.frame()
 
 bulk_sc_cor <- function(i) {
   
@@ -41,6 +58,17 @@ bulk_sc_cor <- function(i) {
   
   samp_tes <- cbind(bulk_tes, sc_tes)
   
+  samp_te_df <- samp_tes
+  samp_te_df$bulk <- bulk_samp
+  samp_te_df$sc <- sc_samp
+  samp_te_df$tissue <- samples$tissue[i]
+  samp_te_df$participant <- samples$participant_id[i]
+  colnames(samp_te_df) <- c("bulk_counts", "sc_counts", "bulk", "sc", "tissue",
+                            "participant")
+  row.names(samp_te_df) <- NULL
+  
+  all_bulk_sc <<- rbind(all_bulk_sc, samp_te_df)
+  
   cor.test(samp_tes[,1], samp_tes[,2], 
            method="spearman", exact = FALSE)
   
@@ -48,11 +76,26 @@ bulk_sc_cor <- function(i) {
             add = "reg.line", conf.int = TRUE, 
             cor.coef = TRUE, cor.method = "spearman",
             exact=FALSE,
+            color=samples$color[samples$bulk_RNAseq == bulk_samp],
             xlab = paste(bulk_samp, "Bulk", sep = " "), 
             ylab = paste(sc_samp, "SC", sep = " "))
+  
 }
 
 cowplot::plot_grid(plotlist = lapply(seq_along(samples$bulk_RNAseq), bulk_sc_cor), 
                    ncol=5)
 
 ggsave("plots/sample_level_bulk_sc_corr.pdf", height=20, width=20)
+
+########################### SC / BULK CORRELATION ALL ##########################
+
+ggscatter(all_bulk_sc, x = "bulk_counts", y = "sc_counts", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "spearman",
+          exact=FALSE,
+          xlab = "Bulk CPM", 
+          ylab = "Single Cell CPM") +
+  font("xlab", size=10) +
+  font("ylab", size=10)
+
+ggsave("plots/all_bulk_sc_corr.pdf", height=4, width=4)
