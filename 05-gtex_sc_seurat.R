13a333a58b1162dfb89472c862d309bc3cc6f869
Bhavya Singh <singhb39@mcmaster.ca>
2022-09-23 05:56:58 -0400

    seurat for new sample, plus outputting full .tx matrices for pseudpbulk as well

--- 05-gtex_sc_seurat.R
+++ 05-gtex_sc_seurat.R
@@ -64,12 +64,12 @@ JackStrawPlot(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:15)
 
 # Cluster the cells
 GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
-  FindNeighbors(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:10)
+  FindNeighbors(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:6)
 GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
   FindClusters(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, resolution = 0.5)
 
 GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
-  RunUMAP(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:10)
+  RunUMAP(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:6)
 
 DimPlot(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, reduction = "umap")
 
@@ -90,7 +90,7 @@ GTEX_1MCC2_5013_SM_HPJ3D.markers %>%
 
 GTEX_1MCC2_5013_SM_HPJ3D.markers %>%
   group_by(cluster) %>%
-  top_n(n = 10, wt = avg_log2FC) -> top10
+  top_n(n = 30, wt = avg_log2FC) -> top10
 DoHeatmap(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, features = top10$gene) + NoLegend()
 
 ####################### GTEX_1CAMS_5015_SM_HPJ3C (Breast) ######################
@@ -135,7 +135,7 @@ JackStrawPlot(GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm, dims = 1:15)
 
 # Cluster the cells
 GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm <- 
-  FindNeighbors(GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm, dims = 1:5)
+  FindNeighbors(GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm, dims = 1:4)
 GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm <- 
   FindClusters(GTEX_1CAMS_5015_SM_HPJ3C.seurat.norm, resolution = 0.5)
 
@@ -195,4 +195,198 @@ VizDimLoadings(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:2, reduction = "pc
 
 DimHeatmap(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:5, cells = 1000, balanced = TRUE)
 
+# Determine the ‘dimensionality’ of the dataset
 
+GTEX_1R9PN_5002_SM_HD2MC.seurat.norm <- 
+  JackStraw(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, num.replicate = 100)
+GTEX_1R9PN_5002_SM_HD2MC.seurat.norm <- 
+  ScoreJackStraw(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:20)
+
+JackStrawPlot(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:15)
+
+# Choosing 10 PCs
+
+# Cluster the cells
+GTEX_1R9PN_5002_SM_HD2MC.seurat.norm <- 
+  FindNeighbors(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:10)
+GTEX_1R9PN_5002_SM_HD2MC.seurat.norm <- 
+  FindClusters(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, resolution = 0.5)
+
+GTEX_1R9PN_5002_SM_HD2MC.seurat.norm <- 
+  RunUMAP(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, dims = 1:5)
+
+DimPlot(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, reduction = "umap")
+
+# Finding differentially expressed features (cluster biomarkers)
+
+cluster2.markers <- FindMarkers(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, 
+                                ident.1 = 2, min.pct = 0.25)
+head(cluster2.markers, n = 5)
+
+# find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+GTEX_1R9PN_5002_SM_HD2MC.markers <- 
+  FindAllMarkers(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, 
+                 only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+GTEX_1R9PN_5002_SM_HD2MC.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+GTEX_1R9PN_5002_SM_HD2MC.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top10
+DoHeatmap(GTEX_1R9PN_5002_SM_HD2MC.seurat.norm, features = top10$gene) + NoLegend()
+
+
+####################### GTEX_1MCC2_5013_SM_HPJ3D (Breast) ######################
+
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  FindVariableFeatures(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, 
+                       selection.method = "vst",
+                       nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm), 30)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+# Scale data
+all.genes <- rownames(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm)
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  ScaleData(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, features = all.genes)
+
+# Perform linear dimensional reduction
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <-
+  RunPCA(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, 
+         features = VariableFeatures(object = GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm))
+
+print(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm[["pca"]], dims = 1:5, nfeatures = 5)
+
+VizDimLoadings(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:2, reduction = "pca")
+
+DimHeatmap(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:5, cells = 1000, balanced = TRUE)
+
+# Determine the ‘dimensionality’ of the dataset
+
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  JackStraw(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, num.replicate = 100)
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  ScoreJackStraw(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:20)
+
+JackStrawPlot(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:15)
+
+# Choosing 11 PCs
+
+# Cluster the cells
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  FindNeighbors(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:6)
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  FindClusters(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, resolution = 0.5)
+
+GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm <- 
+  RunUMAP(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, dims = 1:6)
+
+DimPlot(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, reduction = "umap")
+
+# Finding differentially expressed features (cluster biomarkers)
+
+cluster2.markers <- FindMarkers(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, 
+                                ident.1 = 2, min.pct = 0.25)
+head(cluster2.markers, n = 5)
+
+# find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+GTEX_1MCC2_5013_SM_HPJ3D.markers <- 
+  FindAllMarkers(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, 
+                 only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+GTEX_1MCC2_5013_SM_HPJ3D.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+GTEX_1MCC2_5013_SM_HPJ3D.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 30, wt = avg_log2FC) -> top10
+DoHeatmap(GTEX_1MCC2_5013_SM_HPJ3D.seurat.norm, features = top10$gene) + NoLegend()
+
+######################## GTEX_1HSMQ_5005_SM_GKSJF (Lung) #######################
+
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  FindVariableFeatures(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, 
+                       selection.method = "vst",
+                       nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top50 <- head(VariableFeatures(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm), 50)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm)
+plot2 <- LabelPoints(plot = plot1, points = top50, repel = TRUE)
+pdf("plots/GTEX_1HSMQ_5005_SM_GKSJF_variable_features.pdf", height=7, width=7)
+plot2
+dev.off()
+
+all.genes <- rownames(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm)
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  ScaleData(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, features = all.genes)
+
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <-
+  RunPCA(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, 
+         features = VariableFeatures(object = GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm))
+
+print(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm[["pca"]], dims = 1:5, nfeatures = 5)
+
+pdf("plots/GTEX_1HSMQ_5005_SM_GKSJF_dim_loadings.pdf", height=6, width=8)
+VizDimLoadings(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:2, reduction = "pca")
+dev.off()
+
+DimHeatmap(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:5, cells = 1000, balanced = TRUE)
+
+# Determine the ‘dimensionality’ of the dataset
+
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  JackStraw(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, num.replicate = 100)
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  ScoreJackStraw(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:20)
+
+JackStrawPlot(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:15)
+
+# Choosing 8 PCs
+
+# Cluster the cells
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  FindNeighbors(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:8)
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  FindClusters(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, resolution = 0.5)
+
+GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm <- 
+  RunUMAP(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, dims = 1:5)
+
+pdf("plots/GTEX_1HSMQ_5005_SM_GKSJF_umap.pdf", height=7, width=7)
+DimPlot(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, reduction = "umap")
+dev.off()
+
+# Finding differentially expressed features (cluster biomarkers)
+
+cluster2.markers <- FindMarkers(GTEX_1HSMQ_5005_SM_GKSJF.markers, 
+                                ident.1 = 2, min.pct = 0.25)
+head(cluster2.markers, n = 5)
+
+# find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+GTEX_1HSMQ_5005_SM_GKSJF.markers <- 
+  FindAllMarkers(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, 
+                 only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+GTEX_1HSMQ_5005_SM_GKSJF.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+GTEX_1HSMQ_5005_SM_GKSJF.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top50
+
+pdf("plots/GTEX_1HSMQ_5005_SM_GKSJF_heatmap.pdf", height=10, width=10)
+DoHeatmap(GTEX_1HSMQ_5005_SM_GKSJF.seurat.norm, features = top50$gene) + NoLegend()
+dev.off()
--- 06-bulk_sc_cor.R
+++ 06-bulk_sc_cor.R
@@ -20,11 +20,14 @@ library(ggpubr)
 load("r_outputs/01-mean_cpm_by_tissue_type.RData")
 load("r_outputs/01-counts.Rdata")
 load("r_outputs/03-mean_raw_scTE_cpm_by_tissue_type.RData")
+load("r_outputs/03-scgtex_seurat_counts.Rdata")
 
 ################################## DATA SETUP ##################################
 
 row.names(samples) <- samples$sn_RNAseq
 samples$sn_RNAseq_names <- gsub("-", "_", samples$sn_RNAseq)
+
+# Keep only genes 
 
 ################################# COLOR SETUP ##################################
 
@@ -41,7 +44,7 @@ samples$color <-
   tissue_color$mycols[match(samples$tissue, tissue_color$tissues)]
 
 ############################# SC / BULK CORRELATION ############################
-################################# SAMPLE LEVEL #################################
+################################ SAMPLE LEVEL TE ###############################
 
 all_bulk_sc <- data.frame()
 
@@ -87,7 +90,7 @@ cowplot::plot_grid(plotlist = lapply(seq_along(samples$bulk_RNAseq), bulk_sc_cor
 
 ggsave("plots/sample_level_bulk_sc_corr.pdf", height=20, width=20)
 
-########################### SC / BULK CORRELATION ALL ##########################
+########################## SC / BULK CORRELATION ALL TE #########################
 
 ggscatter(all_bulk_sc, x = "bulk_counts", y = "sc_counts", 
           add = "reg.line", conf.int = TRUE, 
@@ -99,3 +102,50 @@ ggscatter(all_bulk_sc, x = "bulk_counts", y = "sc_counts",
   font("ylab", size=10)
 
 ggsave("plots/all_bulk_sc_corr.pdf", height=4, width=4)
+
+############################# SC / BULK CORRELATION ############################
+############################### SAMPLE LEVEL GENE ##############################
+
+all_bulk_sc_gene <- data.frame()
+
+bulk_sc_cor_gene <- function(i) {
+  
+  bulk_samp <- samples$bulk_RNAseq[i]
+  sc_samp <- samples$sn_RNAseq_names[i]
+  
+  bulk_tes <- as.data.frame(counts.cpm.rtx[,bulk_samp])
+  colnames(bulk_tes) <- c(bulk_samp)
+  
+  sc_tes <- as.data.frame(pseudobulk.rtx.cpm.raw[,sc_samp, drop=FALSE])
+  colnames(sc_tes) <- c(sc_samp)
+  
+  samp_tes <- cbind(bulk_tes, sc_tes)
+  
+  samp_te_df <- samp_tes
+  samp_te_df$bulk <- bulk_samp
+  samp_te_df$sc <- sc_samp
+  samp_te_df$tissue <- samples$tissue[i]
+  samp_te_df$participant <- samples$participant_id[i]
+  colnames(samp_te_df) <- c("bulk_counts", "sc_counts", "bulk", "sc", "tissue",
+                            "participant")
+  row.names(samp_te_df) <- NULL
+  
+  all_bulk_sc <<- rbind(all_bulk_sc, samp_te_df)
+  
+  cor.test(samp_tes[,1], samp_tes[,2], 
+           method="spearman", exact = FALSE)
+  
+  ggscatter(samp_tes, x = bulk_samp, y = sc_samp, 
+            add = "reg.line", conf.int = TRUE, 
+            cor.coef = TRUE, cor.method = "spearman",
+            exact=FALSE,
+            color=samples$color[samples$bulk_RNAseq == bulk_samp],
+            xlab = paste(bulk_samp, "Bulk", sep = " "), 
+            ylab = paste(sc_samp, "SC", sep = " "))
+  
+}
+
+cowplot::plot_grid(plotlist = lapply(seq_along(samples$bulk_RNAseq), bulk_sc_cor), 
+                   ncol=5)
+
+ggsave("plots/sample_level_bulk_sc_corr.pdf", height=20, width=20)
